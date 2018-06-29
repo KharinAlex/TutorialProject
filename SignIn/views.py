ffe39a56f8454fce4f8b7cf5eb03f757a9cfdb98
KharinAlex <kharin_aa@mail.ua>
2018-06-30 01:09:48 +0300

    Was made wrong revert
    This reverts commit 16c6c0eef0c223ed552d3ee11e15a3a3f96295cc.

---
+++ SignIn/views.py
@@ -0,0 +1,48 @@
+from django.views.generic.edit import FormView
+from django.views.generic.base import View
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
+
+
+
+class RegisterFormView(FormView):
+    form_class = UserCreationForm
+
+    success_url = "/auth/login/"
+
+    # Шаблон, который будет использоваться при отображении представления.
+    template_name = "SignIn/registration.html"
+
+    def form_valid(self, form):
+        # Создаём пользователя, если данные в форму были введены корректно.
+        form.save()
+
+        # Вызываем метод базового класса
+        return super(RegisterFormView, self).form_valid(form)
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+
+    # Аналогично регистрации, только используем шаблон аутентификации.
+    template_name = "SignIn/login.html"
+
+    # В случае успеха перенаправим на главную.
+    success_url = "/"
+
+    def form_valid(self, form):
+        # Получаем объект пользователя на основе введённых в форму данных.
+        self.user = form.get_user()
+
+        # Выполняем аутентификацию пользователя.
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+
+        # После чего, перенаправляем пользователя на главную страницу.
+        return HttpResponseRedirect("/")
